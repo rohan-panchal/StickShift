9e9ca5cee7b2ac5a4e7bbfd6d852a70b6fbf5398
Rohan Panchal <rohan@rohanpanchal.com>
2016-04-12 18:24:46 -0500

    Updated requirements with nose and coverage for testing.
    Updated setup.py with proper nose tests collector.

--- setup.py
+++ setup.py
@@ -9,4 +9,6 @@ setup(name='StickShift',
       install_requires=['psycopg2==2.6.1', 'click==6.2', 'natsort==4.0.4'],
       packages=['stickshift'],
       entry_points={'console_scripts': ["stickshift=stickshift.shell:shell"]},
+      test_suite="nose.collector",
+      tests_require=["nose"]
       )
